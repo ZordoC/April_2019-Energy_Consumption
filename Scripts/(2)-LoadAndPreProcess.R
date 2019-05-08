5fa434f90faa610a7e412f5e1c48e20f5ea069ee
Zordo <pedrogodinhoc@gmail.com>
2019-05-08 12:59:00 +0200

    LongFormat

--- Scripts/(2)-LoadAndPreProcess.R
+++ Scripts/(2)-LoadAndPreProcess.R
@@ -36,9 +36,14 @@ FullYears <- FullYears[,c(ncol(FullYears), 1:(ncol(FullYears)-1))]
 
 FullYears$DateTime <- as.POSIXct(FullYears$DateTime, "%Y/%m/%d %H:%M:%S")
 
-# attr(FullYears$DateTime, "tzone") <- "Europe/Paris"
+ attr(FullYears$DateTime, "tzone") <- "Europe/Lisbon"
 
 FullYears <- pad(FullYears,break_above =  3)
+
+
+library(zoo)
+
+FullYears <- as.data.frame(lapply(FullYears, na.aggregate))
 
 
 #### The Data-Sets ####
@@ -46,25 +51,24 @@ FullYears <- pad(FullYears,break_above =  3)
 FullYears <- pre_function(FullYears)
 
 
-na_check_test_set <-FullYears %>% filter(year == 2008)  
-
-sum(is.na(na_check_test_set))
-
-na_check_test_set[which(TRUE),] # No NA'S on test-set ! great ! 
-
-
-library(zoo)
-
-FullYears <- lapply(FullYears, na.aggregate)
-
-
+# na_check_test_set <-FullYears %>% filter(year == 2008)  
+# 
+# sum(is.na(na_check_test_set))
+# 
+# na_check_test_set[which(TRUE),] 
 # FullYears <- change_names(FullYears)
 
 # FullYears <-ConversionFunction(FullYears)
 
 #### Longformat #### 
-# FullYears_tidy <- FullYears %>%
-#                   gather(Meter, KWatt_hr, `Kitchen`, `LaundryRoom`, `Heat`)  %>%  factor(FullYears_tidy$Meter)
-# 
-# 
+ FullYears_tidy <- FullYears %>%
+                   tidyr::  gather(Meter, KWatt_hr, `Kitchen`, `LaundryRoom`, `Heat`)  %>%  factor(FullYears_tidy$Meter)
+ 
+ 
+
+
+Full_Years_long <- FullYears %>%  melt(id.vars= -c(2,3,4,5))
+
+
+Full_Years_long <- Full_Years_long[,-c(10,11)]
 # FullYears_tidy <- pre_function(FullYears_tidy)
